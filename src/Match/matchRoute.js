d5145f478aabb7775e09cfefc42657ef89e48710
CYY1007 <younguk86@gmail.com>
2022-11-13 06:20:05 +0900

    jwt middleware 각 middleware에 추가

--- src/Match/matchRoute.js
+++ src/Match/matchRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const jwtMiddleware = require('../jwtMiddleware');
 const router = express.Router();
 const matchController = require('./matchController');
 
-router.get('/', matchController.test)
+router.get('/',matchController.test)
 router.post('/', matchController.match);
 router.get('/:id', matchController.getProfile);
 
