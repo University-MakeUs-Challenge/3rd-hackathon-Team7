109bfe54dfd0211123a32c8221a664135232eaf9
BangChaeul <30cmlive@naver.com>
2022-11-13 04:54:29 +0900

    null

--- config/express.js
+++ config/express.js
@@ -1,6 +1,11 @@
 import compression from "compression";
 import express from "express"
 import cors from "cors"
+
+
+
+
+
 import match from "../src/Match/matchRoute"
 
 const app = express();
@@ -9,6 +14,10 @@ app.use(compression());
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors());
+
+
+
+
 
 app.use('/match', match);
 
